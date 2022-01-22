4c8280ea41a61367a61d92e7d28152ff6fc1d710
Gustavo Matheus Morinaga Cardoso <gustavo.morinaga27@gmail.com>
2022-01-22 14:47:47 -0300

    fix: reponsive

--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -2,12 +2,15 @@ import { useRef } from 'react';
 import {
 	Avatar,
 	Center,
+	Divider,
 	Flex,
 	Heading,
 	HStack,
 	Link,
 	Stack,
 	Text,
+	Wrap,
+	WrapItem,
 } from '@chakra-ui/react';
 
 // --- Interfaces ---
@@ -29,7 +32,7 @@ export default function UserComponent({ user }: IUser_Page): JSX.Element {
 		<MotionBox w="full" h="full" display="grid" placeItems="center" ref={constraintsRef}>
 			<MotionBox
 				maxW="2xl"
-				p={2}
+				p={4}
 				bgColor="gray.600"
 				borderWidth="thin"
 				borderColor="gray.700"
@@ -54,17 +57,23 @@ export default function UserComponent({ user }: IUser_Page): JSX.Element {
 					<Stack>
 						<Stack spacing={1}>
 							{user.name && (
-								<Heading size="lg" lineHeight="none" color="purple.400">
+								<Heading
+									size="lg"
+									lineHeight="none"
+									color="purple.400"
+									textAlign={{ base: 'center', md: 'left' }}
+								>
 									{user.name}
 								</Heading>
 							)}
 							<Link
 								href={user.html_url}
 								isExternal
-								w="fit-content"
+								w={{ base: 'full', md: 'fit-content' }}
 								display="flex"
 								alignItems="center"
 								gap={1}
+								justifyContent={{ base: 'center', md: 'left' }}
 								color="gray.300"
 								_hover={{ color: 'white' }}
 							>
@@ -72,63 +81,78 @@ export default function UserComponent({ user }: IUser_Page): JSX.Element {
 								{user.login}
 							</Link>
 						</Stack>
-						<Stack direction={['column', 'row']}>
-							<Flex color="white">
-								<Center
-									gap={1}
-									bgColor="green.400"
-									borderWidth="thin"
-									borderColor="green.500"
-									borderRadius="md"
-									px={2}
-								>
-									<FiUsers />
-									<HStack divider={<Text mx={1}>·</Text>}>
-										<HStack>
-											<Text fontSize="sm">
-												<Text as="strong">{user.followers}</Text> followers
-											</Text>
-										</HStack>
-										<HStack>
-											<Text fontSize="sm">
-												<Text as="strong">{user.following}</Text> following
-											</Text>
-										</HStack>
-									</HStack>
-								</Center>
-							</Flex>
-							<Flex color="white">
-								<Center
-									gap={1}
-									bgColor="yellow.400"
-									borderWidth="thin"
-									borderColor="yellow.500"
-									borderRadius="md"
-									px={2}
-								>
-									<FiBook />
-									<Text as="strong" fontSize="sm">
-										{user.public_repos}
-									</Text>
-								</Center>
-							</Flex>
-							{user.location && (
+						<Wrap justify={{ base: 'center', md: 'left' }}>
+							<WrapItem>
 								<Flex color="white">
 									<Center
 										gap={1}
-										bgColor="blue.400"
+										bgColor="green.400"
 										borderWidth="thin"
-										borderColor="blue.500"
+										borderColor="green.500"
 										borderRadius="md"
 										px={2}
+										h="6"
 									>
-										<FiMapPin />
-										<Text fontSize="sm">{user.location}</Text>
+										<FiUsers />
+										<HStack divider={<Text mx={1}>·</Text>}>
+											<HStack>
+												<Text fontSize="sm">
+													<Text as="strong">{user.followers}</Text> followers
+												</Text>
+											</HStack>
+											<HStack>
+												<Text fontSize="sm">
+													<Text as="strong">{user.following}</Text> following
+												</Text>
+											</HStack>
+										</HStack>
 									</Center>
 								</Flex>
+							</WrapItem>
+							<WrapItem>
+								<Flex color="white">
+									<Center
+										gap={1}
+										bgColor="yellow.400"
+										borderWidth="thin"
+										borderColor="yellow.500"
+										borderRadius="md"
+										px={2}
+										h="6"
+									>
+										<FiBook />
+										<Text as="strong" fontSize="sm">
+											{user.public_repos}
+										</Text>
+									</Center>
+								</Flex>
+							</WrapItem>
+							{user.location && (
+								<WrapItem>
+									<Flex color="white">
+										<Center
+											gap={1}
+											bgColor="blue.400"
+											borderWidth="thin"
+											borderColor="blue.500"
+											borderRadius="md"
+											px={2}
+											h="6"
+										>
+											<FiMapPin />
+											<Text fontSize="sm">{user.location}</Text>
+										</Center>
+									</Flex>
+								</WrapItem>
 							)}
-						</Stack>
-						{user.bio && <Text color="white">{user.bio}</Text>}
+						</Wrap>
+
+						{user.bio && (
+							<>
+								<Divider orientation="horizontal" />
+								<Text color="white">{user.bio}</Text>
+							</>
+						)}
 					</Stack>
 				</Stack>
 			</MotionBox>
