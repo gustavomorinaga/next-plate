ffeb0c7d91f4b04020b23b5e86f1054a1121d017
Gustavo Matheus Morinaga Cardoso <47375774+gmatthewsfeuer@users.noreply.github.com>
2022-01-26 20:43:12 -0300

    fix: open graph seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,12 +1,18 @@
 export default {
 	openGraph: {
 		type: 'website',
-		locale: 'pt_br',
-		site_name: 'SiteName',
+		locale: 'en_US',
+		url: 'https://next-plate.vercel.app',
+		site_name: 'Next-Plate',
+		description: '🦸‍♀️ A super template for Next.js with a pack of incredible tools',
+		image: 'https://next-plate.vercel.app/assets/images/banner.jpg',
 	},
 	twitter: {
-		handle: '@handle',
-		site: '@site',
-		cardType: 'summary_large_image',
+		domain: 'https://next-plate.vercel.app',
+		title: 'Next-Plate',
+		description: '🦸‍♀️ A super template for Next.js with a pack of incredible tools',
+		creator: '@GustavoMorinaga',
+		image: 'https://next-plate.vercel.app/assets/images/banner.jpg',
+		alt: 'Banner of Next-Plate website',
 	},
 };
