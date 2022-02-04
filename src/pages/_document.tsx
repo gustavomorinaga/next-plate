afd6c2781107b6f4d7bd277160c6242fbfa129a2
Gustavo Matheus Morinaga Cardoso <47375774+gmatthewsfeuer@users.noreply.github.com>
2022-02-04 12:59:55 -0300

    fix: theme link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,7 @@
 import Document, { DocumentProps, Html, Head, Main, NextScript } from 'next/document';
 import { ColorModeScript } from '@chakra-ui/react';
 
-import theme from '../../theme';
+import theme from '@styles/theme';
 
 class MyDocument extends Document<DocumentProps> {
 	render(): JSX.Element {
