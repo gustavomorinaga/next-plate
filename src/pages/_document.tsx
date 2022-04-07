685892337b29341409168038d7b8e6a54b11b239
Gustavo Matheus Morinaga Cardoso <gustavo.morinaga27@gmail.com>
2022-04-07 00:28:39 -0300

    Remove unused import in _document.tsx
    Remove unused import of Document and DocumentProps

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { DocumentProps, Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 import { ColorModeScript } from '@chakra-ui/react';
 
 // --- Styles ---
