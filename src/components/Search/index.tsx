3cd8a9df413a4b83306c87d02d06f1ee5d726b07
Gustavo Matheus Morinaga Cardoso <gustavo.morinaga27@gmail.com>
2022-06-30 17:20:45 -0300

    fix: is invalid error type

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -51,7 +51,7 @@ export default function SearchComponent({
 		<MotionBox w="full">
 			<form onSubmit={handleSubmit(onSubmit)}>
 				<HStack spacing="2" w="full" alignItems="flex-start" py="2">
-					<FormControl isInvalid={errors.login}>
+					<FormControl isInvalid={!!errors.login}>
 						<InputGroup size="lg" variant="outline" borderColor="gray.600">
 							<InputLeftElement
 								pointerEvents="none"
