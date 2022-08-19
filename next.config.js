743ecf4cb4c99b05f46e13adf050376e94e15e26
Gustavo Matheus Morinaga Cardoso <gustavo.morinaga27@gmail.com>
2022-08-19 01:10:24 -0300

    fix: problems with manifest and field error

--- next.config.js
+++ next.config.js
@@ -6,64 +6,64 @@ const runtimeCaching = require('next-pwa/cache');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	esModule: true,
-	// swcMinify: true,
-	// images: {
-	// 	formats: ['image/avif', 'image/webp'],
-	// 	domains: ['avatars.githubusercontent.com'],
-	// },
-	// extends: [],
-	rewrites() {
-		const notakuDocsUrlWithBasePath = new URL(
-			`https://1164a4c6-b7b3-4bc3.docs-base-path.notaku.site/docs`
-		).origin;
-		return [
-			{
-				source: '/docs',
-				destination: `${notakuDocsUrlWithBasePath}/docs`,
-			},
-			{
-				source: '/docs/:path*',
-				destination: `${notakuDocsUrlWithBasePath}/docs/:path*`,
-			},
-		];
-	},
+module.exports = async _phase => {
+	/** @type {import('next').NextConfig} */
+	const nextConfig = {
+		reactStrictMode: true,
+		swcMinify: false,
+		// extends: [],
+		// images: {
+		// 	formats: ['image/avif', 'image/webp'],
+		// 	domains: ['avatars.githubusercontent.com'],
+		// },
+		rewrites() {
+			const notakuDocsUrlWithBasePath = new URL(
+				`https://1164a4c6-b7b3-4bc3.docs-base-path.notaku.site/docs`
+			).origin;
+			return [
+				{
+					source: '/docs',
+					destination: `${notakuDocsUrlWithBasePath}/docs`,
+				},
+				{
+					source: '/docs/:path*',
+					destination: `${notakuDocsUrlWithBasePath}/docs/:path*`,
+				},
+			];
+		},
+	};
+
+	return withPlugins(
+		[
+			[
+				withImages,
+				{
+					inlineImageLimit: false,
+					exclude: path.resolve(__dirname, 'public/static/svg'),
+					webpack(config) {
+						config.module.rules.push({
+							test: /\.svg$/,
+							use: ['@svgr/webpack'],
+						});
+
+						return config;
+					},
+				},
+			],
+			[
+				withPWA,
+				{
+					pwa: {
+						dest: 'public',
+						register: true,
+						skipWaiting: true,
+						runtimeCaching,
+						buildExcludes: [/middleware-manifest.json$/],
+						disable: !isProduction,
+					},
+				},
+			],
+		],
+		nextConfig
+	)(_phase, { defaultConfig: {} });
 };
-
-module.exports = withPlugins(
-	[
-		[
-			withImages,
-			{
-				inlineImageLimit: false,
-				exclude: path.resolve(__dirname, 'public/static/svg'),
-				webpack(config) {
-					config.module.rules.push({
-						test: /\.svg$/,
-						use: ['@svgr/webpack'],
-					});
-
-					return config;
-				},
-			},
-		],
-		[
-			withPWA,
-			{
-				pwa: {
-					disable: !isProduction,
-					dest: 'public',
-					register: true,
-					skipWaiting: true,
-					runtimeCaching,
-
-					// Prevent Bad Pre-Caching Error:
-					// buildExcludes: [/middleware-manifest\.json$/],
-				},
-			},
-		],
-	],
-	nextConfig
-);
